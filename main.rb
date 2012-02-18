035163db439f13ee9db0fee174135ae875302961
H4y4shi <serpentinexcubed@gmail.com>
2012-02-18 10:47:20 -0600

    third commit

--- main.rb
+++ main.rb
@@ -11,24 +11,40 @@ class Grid
 end
 #800 #640
 class LevelEditorWindow < Gosu::Window
+    attr_reader :x, :y
+    
   def initialize
     super(800, 640, false)
     self.caption = "Negfaron."
     @background_image = Gosu::Image.new(self, "media/GrayBackground640x480.png", true)
     @cursor = Gosu::Image.new(self, "media/Cursor.png", false)
     @grass = GrassTile.new(self)
-    @grass.warp(780, 20)
+    @grass.warp(760, 0)
     @dirt = DirtTile.new(self)
-    @dirt.warp(740, 20)
+    @dirt.warp(720, 0)
     @water = WaterTile.new(self)
-    @water.warp(700,20)
+    @water.warp(680, 0)
   end
+  
+  def under_point?(mouse_x, mouse_y)
+    mouse_x > x and mouse_x < x + width and
+    mouse_y > y and mouse_y < y + height
+  end
+  
   def draw
     @background_image.draw(0,0, ZOrder::Background)
     @cursor.draw(mouse_x, mouse_y, ZOrder::Mouse)
     @grass.draw
     @dirt.draw
     @water.draw
+  end
+end
+
+def button_down(id)
+  if id == Gosu::MsLeft then
+    if grass.image.underpoint?
+      Process.exit
+    end
   end
 end
 
--- tileclasses.rb
+++ tileclasses.rb
@@ -1,11 +1,12 @@
 class GrassTile
   attr_accessor :x, :y
   def initialize(window)
+    @x = x
+    @y = y
     @image = Gosu::Image.new(window, "media/grass.png", false)
-    @x = @y = @vel_x = @vel_y = @angle = 0.0
   end
   def draw
-    @image.draw_rot(@x, @y, ZOrder::UI, @angle)
+    @image.draw(@x, @y, ZOrder::UI)
   end
   def warp(x, y)
     @x, @y = x, y
@@ -15,11 +16,12 @@ end
 class DirtTile
   attr_accessor :x, :y
   def initialize(window)
+    @x = x
+    @y = y
     @image = Gosu::Image.new(window, "media/dirt.png", false)
-    @x = @y = @vel_x = @vel_y = @angle = 0.0
   end
   def draw
-    @image.draw_rot(@x, @y, ZOrder::UI, @angle)
+    @image.draw(@x, @y, ZOrder::UI)
   end
   def warp(x, y)
     @x, @y = x, y
@@ -29,11 +31,12 @@ end
 class WaterTile
   attr_accessor :x, :y
   def initialize(window)
+    @x = x
+    @y = y
     @image = Gosu::Image.new(window, "media/water.png", false)
-    @x = @y = @vel_x = @vel_y = @angle = 0.0
   end
   def draw
-    @image.draw_rot(@x, @y, ZOrder::UI, @angle)
+    @image.draw(@x, @y, ZOrder::UI)
   end
   def warp(x, y)
     @x, @y = x, y
