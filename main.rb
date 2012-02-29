483201888c8e7ef3f9ff422cc8ce5932a66297c7
H4y4shi <serpentinexcubed@gmail.com>
2012-02-29 06:33:45 -0600

    Cleaned Code, and Various buttons now inherit from button.

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require './tileclasses.rb'
 include Gosu
 
 module ZOrder
-  Background, Stars, UI, Mouse = *0..3
+  Background, Bottom, UI, Mouse = *0..3
 end
 
 class LevelEditorWindow < Gosu::Window
@@ -13,25 +13,27 @@ class LevelEditorWindow < Gosu::Window
   def initialize
     super(800, 640, false)
     self.caption = "Level Editor."
+    
     @background_image = Gosu::Image.new(self, "media/GrayBackground640x480.png", true)
-    @grass_tile = GrassTile.new(self)
-    @grasses = Array.new
     @cursor = Gosu::Image.new(self, "media/Cursor.png", false)
+    
     @grass_button = GrassButton.new(self)
-    @grass_button.warp(760, 0)
-    @dirt_button = DirtButton.new(self)
-    @dirt_button.warp(720, 0)
     @water_button = WaterButton.new(self)
+    @dirt_button = DirtButton.new(self)
+    @end_button = EndButton.new(self) 
+ 
+    @grass_button.warp(760, 0)
     @water_button.warp(680, 0)
-    @end_button = EndButton.new(self)
+    @dirt_button.warp(720, 0)
     @end_button.warp(640, 0)
+
+    @grasses = Array.new
   end
 
   def update
     if button_down? Gosu::MsLeft and mouse_x >= 0 and mouse_x <= 640 and mouse_y >= 0 and mouse_y <= 480 then
       @grasses.push(GrassTile.new(self))
       @grasses[-1].tile_position(mouse_x, mouse_y)
-      @grasses[-1].draw
     end
   end
   
@@ -50,10 +52,12 @@ end
   def draw
     @background_image.draw(0,0, ZOrder::Background)
     @cursor.draw(mouse_x, mouse_y, ZOrder::Mouse)
+    
     @grass_button.draw
     @dirt_button.draw
     @water_button.draw
     @end_button.draw
+    
     @button_down
     @grasses.each { |grass| grass.draw }
   end
--- tileclasses.rb
+++ tileclasses.rb
@@ -1,61 +1,48 @@
-class GrassButton
+class Button
   attr_accessor :x, :y, :width, :height
-  def initialize(window)
+  def initialize(window, x, y, width, height)
     @x = x
     @y = y
+    @width = width
+    @height = height
+  end
+  def draw
+    @image.draw(@x, @y, ZOrder::UI)
+  end
+  def warp(x, y)
+    @x, @y = x, y
+  end
+  def under_point?(mouse_x, mouse_y)
+    mouse_x > @x and mouse_x < @x + 40 and
+    mouse_y > @y and mouse_y < @y + 40
+  end
+end
+
+class GrassButton < Button
+  def initialize(window)
+    super(window, x, y, width, height)
     @image = Gosu::Image.new(window, "media/grass.png", false)
   end
-  def draw
-    @image.draw(@x, @y, ZOrder::UI)
-  end
-  def warp(x, y)
-    @x, @y = x, y
-  end
-  def under_point?(mouse_x, mouse_y)
-    mouse_x > @x and mouse_x < @x + 40 and
-    mouse_y > @y and mouse_y < @y + 40
-  end
 end
 
-class DirtButton
-  attr_accessor :x, :y, :width, :height
+class DirtButton < Button
   def initialize(window)
-    @x = x
-    @y = y
-    @width = width
-    @height = height
+    super(window, x, y, width, height)
     @image = Gosu::Image.new(window, "media/dirt.png", false)
   end
-  def draw
-    @image.draw(@x, @y, ZOrder::UI)
-  end
-  def warp(x, y)
-    @x, @y = x, y
-  end
-  def under_point?(mouse_x, mouse_y)
-    mouse_x > @x and mouse_x < @x + 40 and
-    mouse_y > @y and mouse_y < @y + 40
+end
+
+class WaterButton < Button
+  def initialize(window)
+    super(window, x, y, width, height)
+    @image = Gosu::Image.new(window, "media/water.png", false)
   end
 end
 
-class WaterButton
-  attr_accessor :x, :y, :width, :height
+class EndButton < Button
   def initialize(window)
-    @x = x
-    @y = y
-    @width = width
-    @height = height
-    @image = Gosu::Image.new(window, "media/water.png", false)
-  end
-  def draw
-    @image.draw(@x, @y, ZOrder::UI)
-  end
-  def warp(x, y)
-    @x, @y = x, y
-  end
-  def under_point?(mouse_x, mouse_y)
-    mouse_x > @x and mouse_x < @x + 40 and
-    mouse_y > @y and mouse_y < @y + 40
+    super(window, x, y, width, height)
+    @image = Gosu::Image.new(window, "media/end.png", false)
   end
 end
 
@@ -77,25 +64,6 @@ class GrassTile
   def draw
     @image.draw(x_tile, y_tile, ZOrder::UI)
   end 
-  def warp(x, y)
-    @x, @y = x, y
-  end
-  def under_point?(mouse_x, mouse_y)
-    mouse_x > @x and mouse_x < @x + 40 and
-    mouse_y > @y and mouse_y < @y + 40
-  end
-end
-
-class EndButton
-  attr_accessor :x, :y, :width, :height
-  def initialize(window)
-    @x = x
-    @y = y
-    @image = Gosu::Image.new(window, "media/end.png", false)
-  end
-  def draw
-    @image.draw(@x, @y, ZOrder::UI)
-  end
   def warp(x, y)
     @x, @y = x, y
   end
